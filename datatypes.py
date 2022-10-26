7e72fbb5ee04eb10f9750773fc3f2274b2bda6df
vickykimani <victorianightingale70@gmail.com>
2022-10-26 19:55:55 +0300

    initial commit

---
+++ datatypes.py
@@ -0,0 +1,29 @@
+# datatypes in python
+
+# primitive datatypes
+
+x = 5  # int
+y = 2.5  # float
+
+x, y = 5, 2.5
+
+# python allows variable declarations on the same line
+a = b = c = 3
+
+my_string = ("hello there")  # str data type
+is_true = True  # boolean data type
+is_false = False  # boolean data type
+my_first_list = [1, 2, 3]  # list data type
+none_type = None
+
+my_char = ('a')
+
+print(type(x))
+print(type(y))
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(my_string))
+print(type(my_first_list))
+print(type(is_true))
+print(type(is_false))
