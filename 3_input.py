873b8202bf960da4e72ea2c9de5797fe482596f6
vickykimani <victorianightingale70@gmail.com>
2022-11-02 18:31:02 +0300

    input in python

---
+++ 3_input.py
@@ -0,0 +1,14 @@
+# input in python
+# python uses the function input() to capture and store information from the user.
+
+print("User Profile Application")
+first_name = input("First Name: ")
+last_name = input("Last Name: ")
+occupation = input("Your Job: ")
+
+print("Your first name is: " + first_name)
+print("Your last name is: " + last_name)
+print("Your job is: " + occupation)
+
+
+# another way of outputting information in python is through formatted strings
--- output.py
+++
@@ -1,9 +0,0 @@
-# output in python
-# the print() function is used to display output on the screen
-print("hello world")
-
-# python statements have no terminator
-print(2+2)
-
-
-print(10 % 3)
