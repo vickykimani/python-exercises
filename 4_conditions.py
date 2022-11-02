9c1c2b2a3fa158d1476c879b7d4518c0a827bfd4
vickykimani <victorianightingale70@gmail.com>
2022-11-02 19:03:23 +0300

    conditions in python

--- 4_conditions.py
+++ 4_conditions.py
@@ -1,1 +1,6 @@
 # python uses 'if' statements to evaluate conditions
+driver_age = int(input("Please enter you age: "))
+if (driver_age >= 18):
+    print("Eligible to have a driver's license.")
+else:
+    print("Not eligible to have a driver's license.")
