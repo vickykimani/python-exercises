1411210ca128f770a23284e4407b66d4b56faa0d
vickykimani <victorianightingale70@gmail.com>
2022-11-02 20:07:00 +0300

    loops in python

--- 5_loops.py
+++ 5_loops.py
@@ -1,2 +1,10 @@
 # python implements loops using the 'for' statement
 # accompanied by additional logic
+my_fruits = ["mango", "orange", "peach", "apple"]
+for fruit in my_fruits:
+    print(fruit)
+print(f"The list {my_fruits} has {len(my_fruits)} items.")
+
+my_name = "Jonathan"
+for i in my_name:
+    print(i)
