909972dca8e638eb75bf7c5460beeeee9fa636aa
vickykimani <victorianightingale70@gmail.com>
2022-11-05 18:24:28 +0300

    tuples in python

--- 8_tuples.py
+++ 8_tuples.py
@@ -1,1 +1,20 @@
 # a structure that stores multiple values in one variable
+# a tuple is unchangeable (immutable)
+# a tuple is ordered.
+# a tuple is declared in rounded brackets
+
+# declare and initialize a tuple
+my_cars = ("BMW", "Toyota", "Honda")
+print(my_cars)
+
+# retrieve first item in tuple
+print(my_cars[0])
+
+# check the length of the tuple
+print(len(my_cars))
+
+# add value to tuple
+# my_cars[1] = "Mazda"
+# my_cars[5] = "Nissan"
+
+print(my_cars)
