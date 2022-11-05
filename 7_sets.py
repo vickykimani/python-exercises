9aaf6e0151e6dc0d46766424682e32e952026137
vickykimani <victorianightingale70@gmail.com>
2022-11-05 18:17:37 +0300

    sets in python

--- 7_sets.py
+++ 7_sets.py
@@ -1,1 +1,20 @@
 # create and initialize a set.
+ea_cities = {"Kampala", "Nairobi", "Arusha", "Mombasa", "Nairobi"}
+
+print(ea_cities)
+
+print(f"there are {len(ea_cities)} cities in the set.")
+
+print(type(ea_cities))
+
+ea_cities.add("Kigali")
+print(ea_cities)
+
+west_african_cities = {"Lagos", "Accra"}
+south_african_cities = {"Cape Town", "Durban"}
+
+master_city_list = {"New York"}
+
+master_city_list.update(ea_cities)
+
+print(master_city_list)
